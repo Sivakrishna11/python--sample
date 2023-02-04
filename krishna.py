d17f912d466948a0eab25d993b5db3a704986498
kittu <sivakrishna.gogula5@gmail.com>
2023-02-04 16:13:28 +0530

    updated file

---
+++ krishna.py
@@ -0,0 +1,12 @@
+obj1=234.234345
+print(obj1)
+print("the value is :",obj1)
+print("the id is:",id(obj1))
+print("the dt is:",type(obj1))
+print()
+obj2=435454.5367876889
+print(obj2)
+print("the value is:",obj2)
+print("the id is:",id(obj2))
+print("the dt is:",type(obj2))
+print()
